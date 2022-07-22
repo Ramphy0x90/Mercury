12966c26875d155e60ab6f04a3474dc6489d6833
ramphy0x90 <ramphy11@hotmail.com>
2022-07-22 22:38:33 +0200

    Angular material theme

---
+++ client/src/app/material.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
+const materialModules = [
+  CommonModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatListModule,
+  MatButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    ...materialModules
+  ],
+  exports: [
+    ...materialModules
+  ]
+})
+export class MaterialModule { }
