761b605612836e0d5fbeb324ce9cc165332fffd0
ramphy0x90 <ramphy11@hotmail.com>
2022-07-28 21:35:41 +0200

    Product page

--- client/src/app/containers/product-page/product-page.component.ts
+++ client/src/app/containers/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product-page',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent implements OnInit {
+  product: Product = {
+    id: 1, name: 'Luz led', description: 'Luz led para internos', image: 'https://www.fomei.com/ew/ew_images/image_of_object?ObjectIdentifier=pli:17b95920-d8b5-4340-94d8-658c425d6959&Filter=9c071f19-a887-49a5-b7b5-1fcf62116d80&ImageIndex=4', category: 1, rating: 3.5, tag: '', price: 3021, attributes: null, product_nodes: null 
+  }
 
   constructor() { }
 
