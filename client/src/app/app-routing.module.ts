63dbb3e3c47454f288626718a76d9ee8ecc6fb2d
ramphy0x90 <ramphy11@hotmail.com>
2022-09-11 12:50:44 +0200

    Routes update

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,14 +7,21 @@ import { PageNotFoundComponent } from './containers/page-not-found/page-not-foun
 import { ProductPageComponent } from './containers/product-page/product-page.component';
 import { ProductsAdminComponent } from './containers/admin/products-admin/products-admin.component';
 import { ProductsPageComponent } from './containers/products-page/products-page.component';
+import { DashboardComponent } from './containers/admin/dashboard/dashboard.component';
+import { CategoriesAdminComponent } from './containers/admin/categories-admin/categories-admin.component';
+import { CategoryFormComponent } from './containers/admin/category-form/category-form.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'admin', component: AdminComponent, children: [
+    { path: '', component: DashboardComponent },
     { path: 'products', component: ProductsAdminComponent },
     { path: 'add-product', component: ProductFormComponent },
-    { path: 'edit-product/:id', component: ProductFormComponent }
+    { path: 'edit-product/:id', component: ProductFormComponent },
+    { path: 'categories', component: CategoriesAdminComponent },
+    { path: 'add-category', component: CategoryFormComponent },
+    { path: 'edit-category/:id', component: CategoryFormComponent }
   ] },
   { path: 'product/:id', component: ProductPageComponent },
   { path: 'products', component: ProductsPageComponent},
